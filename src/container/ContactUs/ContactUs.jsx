759a7dc7230ca094c3a4c632502ef5e21d6d145c
IlliaNaumenko <illia.naumenko@gmail.com>
2022-04-24 15:06:18 +0300

    contactUs is Done

--- src/container/ContactUs/ContactUs.jsx
+++ src/container/ContactUs/ContactUs.jsx
@@ -17,7 +17,22 @@ class CotactUs extends React.Component {
   render(){
     return(
       <div className='app__contactUs' id='contacts' style={{backgroundImage: "url(" + this.state.backgroundImage + ")"}} >
-        
+        <div className='telephone'>
+          <p>
+            Олесандр
+          </p>
+          <a href='tel:0956201283'>
+            +38(095)-620-1283
+          </a>
+        </div> 
+        <div className='email'>
+          <p>
+            E-mail
+          </p>
+          <a href='mailto:fma@gmail.com'>
+            fma@gmail.com
+          </a>
+        </div>
       </div>
     )
   }
