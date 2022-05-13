e0f2230dfd1b7fb46d9fcb235139884c79221753
3DMaX123 <3dmax214@gmail.com>
2022-05-13 14:01:04 +0300

    Add protection @fvilers/disable-react-devtools

--- src/container/AboutUs/AboutUs/AboutUsPhone.jsx
+++ src/container/AboutUs/AboutUs/AboutUsPhone.jsx
@@ -8,8 +8,9 @@ const AboutUsPhone = () => {
     <div className='app__aboutusphone'>
         <div className='aboutus-title'>Feed Motion ADS</div>
         <div className='aboutus-description'>Ми команда, що розуміє проблеми бізнесу та їх виклики сьгодення. Тому і глобальною ціллю нашої компанії стали: просування сайтів рекламним та органічним шляхом, організація роботи рекламних кампаній, їх супровід з підготовкою сайту до викликів. </div>
-        <img className="aboutus-palm" src={images.cutpalm} alt="Palm" /> 
-        <img className="aboutus-palm2" src={images.cutpalm} alt="Palm" /> 
+
+          <img className="aboutus-palm" src={images.cutpalm} alt="Palm" /> 
+          <img className="aboutus-palm2" src={images.cutpalm} alt="Palm" /> 
         <div className='aboutusphone_fma'>
             FMA
         </div>
